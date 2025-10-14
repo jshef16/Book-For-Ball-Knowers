a76e0399f0bebe084fbf2e6f8c27762110a46492
Jordan_S <j.shefman@gaminglabs.com>
2025-10-14 14:51:40 -0600

    First commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import DarkVeil from "./Background";
+import HomeScreenText from "./HomeScreenText";
+
+function Home() {   
+    return (
+        <div className="home-screen">
+            <DarkVeil />
+            <HomeScreenText />
+        </div>
+    );
+}
+
+export default Home;
---
+++ frontend/src/components/HomeScreenText.jsx
@@ -0,0 +1,14 @@
+import '../style/HomeScreenText.css';
+
+
+function HomeScreenText() {
+  return (
+    <div className="home-screen-text">
+      <h1 className="home-screen-read">READ</h1>
+      <h1 className="home-screen-everything">EVERYTHING</h1>
+      <p>For the reader who's first love was sport</p>
+    </div>
+  );
+}
+
+export default HomeScreenText;
